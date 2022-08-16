c2a7d3e26ecd8b47bd09a806995f7d8d1129fb46
Mohamad Ramdan Firdaus <mohamad.firdaus@grabtaxi.com>
2022-08-16 22:41:26 +0700

    sequalize

---
+++ config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_NAME,
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  },
+};
