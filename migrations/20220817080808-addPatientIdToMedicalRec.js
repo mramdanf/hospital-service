23296c6956c84d420a97713d01690495eaa6c9fb
Mohamad Ramdan Firdaus <mohamad.firdaus@grabtaxi.com>
2022-08-17 15:09:55 +0700

    add patientId to MedicalRecords table

---
+++ migrations/20220817080808-addPatientIdToMedicalRec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     return queryInterface.addColumn(
+      'MedicalRecords',
+      'patientId',
+      {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Patients',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     return queryInterface.removeColumn(
+      'MedicalRecords',
+      'patientId'
+    )
+  }
+};
