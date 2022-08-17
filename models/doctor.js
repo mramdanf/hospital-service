d09aca24e582a500aa85a99442f90de38ae211aa
Mohamad Ramdan Firdaus <mohamad.firdaus@grabtaxi.com>
2022-08-17 15:18:09 +0700

    seed and association of MedicalRecord

--- models/doctor.js
+++ models/doctor.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       this.hasOne(models.User);
+      this.hasMany(models.MedialRecord)
     }
   }
   Doctor.init({
---
+++ seeders/20220817081243-insertMedicalRec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   return queryInterface.bulkInsert(
+    'MedicalRecords',
+    [
+      {
+        id: 1,
+        diagnos: 'lambung radang',
+        patientId: 1,
+        doctorId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ]
+   )
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     return queryInterface.bulkDelete(
+      'MedicalRecords',
+      {
+        id: 1,
+      }
+     )
+  }
+};
